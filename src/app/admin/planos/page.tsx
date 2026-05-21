b93eede823a97ed0476e8c474796b24cdf8140cd
Hiago Alves <hiagolvs@gmail.com>
2026-05-21 22:28:19 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    Separator is not

--- src/app/admin/planos/page.tsx
+++ src/app/admin/planos/page.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -9,6 +8,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Switch } from "@/components/ui/switch"
+import { Separator } from "@/components/ui/separator"
 import { 
   Zap, 
   ShieldCheck, 
