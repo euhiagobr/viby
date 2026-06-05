304efc7009f4fb2b8c06452ba11ef09505266dac
Hiago Alves <hiagolvs@gmail.com>
2026-06-05 17:20:10 +0000

    critical Protocolo ERR-VIBY-SU4MM9 Users is not defined
    Pendente
    Stack

--- src/app/dashboard/organizacoes/[username]/anuncios/page.tsx
+++ src/app/dashboard/organizacoes/[username]/anuncios/page.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -31,7 +32,10 @@ import {
   Inbox,
   Info,
   Globe,
-  CheckCircle2
+  CheckCircle2,
+  Users,
+  Zap,
+  CreditCard
 } from "lucide-react"
 import {
   Dialog,
