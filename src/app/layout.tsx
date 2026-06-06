0b7fc6c7fb8546f0091bb27c1a3422c690a53e55
Hiago Alves <hiagolvs@gmail.com>
2026-06-06 17:17:25 +0000

    pois nao ta aparecendo no site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,9 +108,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-        <GoogleAdsTag />
       </head>
       <body className="font-body antialiased bg-[#f8fafc] text-[#000000] flex flex-col min-h-screen">
+        <GoogleAdsTag />
         <FirebaseClientProvider>
           <ErrorManagerProvider>
             <GlobalErrorBoundary>
