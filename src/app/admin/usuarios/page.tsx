7ad581e48113cce9b5c365f6d902222e19b6d016
Hiago Alves <hiagolvs@gmail.com>
2026-06-10 14:32:46 +0000

    Mail is not defined     at AdminUsuariosPage (page.tsx:409:34)     at Ob

--- src/app/admin/usuarios/page.tsx
+++ src/app/admin/usuarios/page.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -45,7 +44,8 @@ import {
   ShieldBan,
   AtSign,
   AlertTriangle,
-  Fingerprint
+  Fingerprint,
+  Mail
 } from "lucide-react"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
