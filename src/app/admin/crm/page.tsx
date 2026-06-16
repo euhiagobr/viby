6e9ea28e00fbe3e1725be3391f4394190a8959b1
Hiago Alves <hiagolvs@gmail.com>
2026-06-16 19:57:37 +0000

    VM3722 <anonymous>:1 ReferenceError: Badge is not defined     at page.ts

--- src/app/admin/crm/page.tsx
+++ src/app/admin/crm/page.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
 import { 
   Send, 
   Users, 
