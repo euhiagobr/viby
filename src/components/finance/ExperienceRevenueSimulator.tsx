43cfcb85bcab0d5078d38a627fad03f38f3c5b4b
Hiago Alves <hiagolvs@gmail.com>
2026-07-01 16:05:09 +0000

    error-boundary-callbacks.ts:90 ReferenceError: DialogFooter is not defin

--- src/components/finance/ExperienceRevenueSimulator.tsx
+++ src/components/finance/ExperienceRevenueSimulator.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import * as React from 'react';
@@ -6,6 +5,7 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
